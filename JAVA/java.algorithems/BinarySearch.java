83995aa875c220b1b77bdf5ccf01c72a77346d99
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2025-02-11 00:36:30 -0800

    recursivebinarysearch

--- JAVA/java.algorithems/BinarySearch.java
+++ JAVA/java.algorithems/BinarySearch.java
@@ -2,10 +2,30 @@ public class BinarySearch {
     public static void main(String[] args) {
         int nums[] = { 4, 8, 10, 18, 29, 55 };
         int target = 55;
+        int start = 0;
+        int end = nums.length - 1;
 
         int result = binarysearch(nums, target);
+        int result2 = recursivebinarysearch(nums, target, start, end);
 
+        System.err.println("Target at :- " + result2);
         System.err.println("Target at :- " + result);
+
+    }
+
+    public static int recursivebinarysearch(int nums[], int target, int start, int end) {
+
+        if (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                return recursivebinarysearch(nums, target, mid + 1, end);
+            } else
+                return recursivebinarysearch(nums, target, start, mid - 1);
+        }
+        return -1;
     }
 
     public static int binarysearch(int nums[], int target) {

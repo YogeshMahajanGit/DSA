f2569619d5bc4cebfea3e03f7f40701134b2d777
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2025-02-10 23:30:57 -0800

    Start java

---
+++ JAVA/java.algorithems/LinearSearch.java
@@ -0,0 +1,25 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+
+        int nums[] = { 4, 8, 10, 22, 39, 55 };
+        int target = 13;
+
+        int result = Linearsearch(nums, target);
+
+        if (result != -1)
+            System.out.println("Target Found At :- " + result + " index");
+        else
+            System.out.println("Target Not Found");
+    }
+
+    public static int Linearsearch(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
+
+        return -1;
+    }
+
+}

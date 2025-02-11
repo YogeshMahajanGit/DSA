234836b553610fefbc4ac824419e9313f2cdee9d
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2025-02-11 14:09:25 -0800

    Insertion Sort

--- JAVA/java.sorting/Demo.java
+++ JAVA/java.sorting/Demo.java
@@ -34,10 +34,25 @@ public class Demo {
             nums2[i] = temp;
         }
 
+        // Insertion Sort
+
+        int arr[] = { 5, 88, 9, 3, 22, 1, 4 };
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
         System.out.println();
         System.out.println("Sorted Array : ");
 
-        for (int n : nums2) {
+        for (int n : arr) {
 
             System.out.print(n + " ");
         }
